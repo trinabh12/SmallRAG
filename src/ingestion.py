f3f9c3b62c27ed3593b4f5c3c844b63ee53d2110
Trinabh Mitra <mitratrinabh@gmail.com>
2026-04-01 14:52:11 +0530

    Update ingestion.py

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,8 +1,9 @@
 from pdfminer.high_level import extract_text
 import tabula
 import fitz
-import os
 import PIL.Image
+import io
+import os
 
 class Ingestion:
     def __init__(self, doc_path):
@@ -20,8 +21,23 @@ class Ingestion:
 
     def extract_text(self):
         texts = self.text
+        return texts
 
     def extract_tables(self):
         tables = self.tables
-    
+        return tables
+
+    def extract_images(self):
+        doc = self.doc
+        images = self.images
+
+        counter = 1
+        
+        for image in images:
+            base_image = doc.extract_image(image[0])
+            image_data = base_image["image"]
+            img = PIL.Image.open(io.BytesIO(image_data))
+            extension = base_image['ext']
+            img.save(open(f"./images/image{counter}.{extension}", "wb"))
+            counter+=1
 
