070f1e76faad9d90834dffe696da1c094848fc56
Trinabh Mitra <mitratrinabh@gmail.com>
2026-04-02 12:39:19 +0530

    Update ingestion.py

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,42 +2,64 @@ from pdfminer.high_level import extract_text
 import tabula
 import fitz
 import PIL.Image
+import json
 import io
 import os
 
 class Ingestion:
     def __init__(self, doc_path):
+        self.doc_name = os.path.basename(doc_path)
         self.doc =  fitz.open(doc_path)
         self.text = extract_text(doc_path)
         self.tables = tabula.read_pdf(doc_path, pages='all', encoding='latin-1')
+        self.images = [img for page in self.doc for img in page.get_images()]
 
-        for i in range(len(self.doc)):
-            page = self.doc[i]
-            self.images = page.get_images()
+        os.makedirs(f"data/{self.doc_name}", exist_ok=True)
 
-        #self.images = [page.get_images() for page in self.pdf]
+        self.extracted_info = {
+            "location": {"file_name": os.path.basename(doc_path)},
+            "text": "",
+            "tables": "",
+            "images_paths": ""
+        }
 
-        self.extracted_info = {}
+    def process_text(self):
+        text_data = {'text_data': self.text}
 
-    def extract_text(self):
-        texts = self.text
-        return texts
+        os.makedirs(f"data/{self.doc_name}/text", exist_ok=True)
+        with open(f'data/{self.doc_name}/text/text-data.json', 'w', encoding='utf-8') as f:
+            json.dump(text_data, f, indent=4)
 
-    def extract_tables(self):
+        text_data_path = f"data/{self.doc_name}/text"
+        return text_data_path
+
+    def process_tables(self):
         tables = self.tables
-        return tables
 
-    def extract_images(self):
-        doc = self.doc
+
+        tabular_data_path = f"data/{self.doc_name}/tables"
+        return tabular_data_path
+
+    def process_images(self):
         images = self.images
-
         counter = 1
-        
+
         for image in images:
-            base_image = doc.extract_image(image[0])
+            base_image = self.doc.extract_image(image[0])
             image_data = base_image["image"]
             img = PIL.Image.open(io.BytesIO(image_data))
             extension = base_image['ext']
-            img.save(open(f"./images/image{counter}.{extension}", "wb"))
+            os.makedirs(f"data/{self.doc_name}/images", exist_ok=True)
+            img.save(open(f"data/{self.doc_name}/images/image{counter}.{extension}", "wb"))
             counter+=1
+
+        image_data_path = f"data/{self.doc_name/images}"
+        return image_data_path
+
+    def extract_and_store(self):
+
+
+
+
+
 
