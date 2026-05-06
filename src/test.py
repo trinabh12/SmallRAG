6930a9db4219dd929a3036e733447aac170d9c00
Trinabh Mitra <mitratrinabh@gmail.com>
2026-05-06 11:34:21 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,3 +3,8 @@ from ingestion import Ingestion
 i = Ingestion("sample.pdf")
 
 i.extract_and_store()
+
+from embedding import Embedder
+
+embedder = Embedder("data/metadata_manifest.json")
+embedder.run()
