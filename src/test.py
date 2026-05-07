1ed92ca43a9c02ad85b60e8c0170bac6464318d6
Trinabh Mitra <mitratrinabh@gmail.com>
2026-05-07 12:51:47 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -8,3 +8,14 @@ from embedding import Embedder
 
 embedder = Embedder("data/sample_metadata_manifest.json")
 embedder.run()
+
+
+from database import  VectorDatabase
+
+db = VectorDatabase("sample")
+
+query = "What is this document about?"
+
+results = db.search(query, top_k=3)
+
+db.pretty_print(results)
